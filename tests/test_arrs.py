5a1defe2fd5b9194a5d2d6bf76d1c5108a4aecdc
pozdniakova olga <caramelca07@mail.ru>
2023-04-28 20:03:01 +0200

    Changed tests coverage. Cover = 100 %

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,6 +4,7 @@ import pytest as pytest
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 
 def test_get_empty_array():
@@ -14,3 +15,6 @@ def test_get_empty_array():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4], -3, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], -7, 3) == [1, 2, 3]
